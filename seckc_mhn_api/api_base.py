be4613f4e6a28e8a47ecc143c222f8c7ab0a31eb
Brandon Winchester <brandonwinchester@gmail.com>
2018-03-21 09:53:51 -0500

    remove notes routes :)

--- seckc_mhn_api/api_base.py
+++ seckc_mhn_api/api_base.py
@@ -14,18 +14,6 @@ from flask_cors import CORS
 APP = FlaskAPI(__name__)
 SOCKET_IO_APP = SocketIO(APP)
 
-NOTES = {
-    0: 'do the shopping',
-    1: 'build the codez',
-    2: 'paint the door',
-}
-
-def note_repr(key):
-    return {
-        'url': request.host_url.rstrip('/') + url_for('notes_detail', key=key),
-        'text': NOTES[key]
-    }
-
 #import modules here
 from seckc_mhn_api.auth.controllers import AUTH_MODULE, user_status
 from seckc_mhn_api.geocode.controllers import GEOCODE_MODULE
@@ -46,45 +34,6 @@ APP.register_blueprint(SENSORS_MODULE)
 def manage_security_headers(response):
     response.headers["Server"] = ""
     return response
-
-
-@APP.route("/", methods=['GET', 'POST'])
-@user_status
-def notes_list():
-    """
-    List or create notes.
-    """
-    print("test")
-    print(request.user_active)
-
-    if request.method == 'POST':
-        note = str(request.data.get('text', ''))
-        idx = max(NOTES.keys()) + 1
-        NOTES[idx] = note
-        return note_repr(idx), status.HTTP_201_CREATED
-
-    # request.method == 'GET'
-    return [note_repr(idx) for idx in sorted(NOTES.keys())]
-
-
-@APP.route("/<int:key>/", methods=['GET', 'PUT', 'DELETE'])
-def notes_detail(key):
-    """
-    Retrieve, update or delete note instances.
-    """
-    if request.method == 'PUT':
-        note = str(request.data.get('text', ''))
-        NOTES[key] = note
-        return note_repr(key)
-
-    elif request.method == 'DELETE':
-        NOTES.pop(key, None)
-        return '', status.HTTP_204_NO_CONTENT
-
-    # request.method == 'GET'
-    if key not in NOTES:
-        raise exceptions.NotFound()
-    return note_repr(key)
 
 if __name__ == "__main__":
     SOCKET_IO_APP.run(APP)
